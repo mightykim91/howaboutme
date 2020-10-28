fe45f9d321b14b78cb8e5704e8540e757f87ace8
rsm4282@naver.com <rsm4282@naver.com>
2020-10-28 17:03:29 +0900

    Feat user nickname duplication check

--- backend/backend/accounts/views.py
+++ backend/backend/accounts/views.py
@@ -4,6 +4,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 
 from .serializers import UserSerializer, ProfileSerializer
+from .models import User, Profile
 
 
 @api_view(['POST'])
@@ -13,3 +14,15 @@ def set_profile(request):
     if serializer.is_valid(raise_exception=True):
         serializer.save(user=request.user)
         return Response(serializer.data)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def dup_check(request):
+    nick = request.data['nickname'].strip()
+    profile = Profile.objects.filter(nickname=nick)
+
+    if len(profile) == 0:
+        return Response(status=200, data='true')
+    else:
+        return Response(status=401, data='false')
