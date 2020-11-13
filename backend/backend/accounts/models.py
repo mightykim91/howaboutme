ed2e2884c2200edfe69f684668ec6ca6b4e44c78
dkqyqyt <rsm4282@naver.com>
2020-11-13 16:14:53 +0900

    Modify accounts model

--- backend/backend/accounts/models.py
+++ backend/backend/accounts/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     email=models.EmailField(unique=False)
-    name = models.CharField(max_length=10)
+    name = models.CharField(max_length=30)
     profile_saved = models.IntegerField(default=0)
     image_saved = models.IntegerField(default=0)
     similarity = models.IntegerField(default=0)
