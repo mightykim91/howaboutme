a7648161e154f543e420ddbd1df6cb2d24fb5b56
dkqyqyt <rsm4282@naver.com>
2020-11-16 01:36:54 +0900

    Modify user model

--- backend/backend/accounts/models.py
+++ backend/backend/accounts/models.py
@@ -4,7 +4,7 @@ from django.conf import settings
 
 class User(AbstractUser):
     email=models.EmailField(unique=False)
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=150)
     profile_saved = models.IntegerField(default=0)
     image_saved = models.IntegerField(default=0)
     similarity = models.IntegerField(default=0)
